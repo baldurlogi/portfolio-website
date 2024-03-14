b166fc1d2285d737ed0adc6802ac0852cbd088ed
BaldurLogi <baldurlogi16@icloud.com>
2024-03-14 19:00:42 +0100

    fix the mailto address

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,7 +33,7 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
+        <a className="underline" href="mailto:baldurlogi16@icloud.com">
           baldurlogi16@icloud.com
         </a>{" "}
         or through this form.
